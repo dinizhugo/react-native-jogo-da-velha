840f3ded4506acb2b0781a4c8cba6ac32a4b1d64
dinizhugo <hugodiniztech01@gmail.com>
2025-04-30 12:46:51 -0300

    feat: Adicionando o scoreBoard

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
 import { Text, View, StyleSheet, Alert, TouchableOpacity } from "react-native";
 import CustomButton from "@/componentes/custom_button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type Squares = (string | null)[];
 
 export default function Index() {
   const [gameMatriz, setGameMatriz] = useState<Squares>(Array(9).fill(null));
   const [isXNext, setIsXNext] = useState<boolean>(true);
+  const [playerVictory, setPlayerVictory] = useState<number>(0);
+  const [cpuVictory, setCpuVictory] = useState<number>(0);
 
   const checkWinner = (squares: Squares): string | null => {
     const lines = [
@@ -29,21 +31,44 @@ export default function Index() {
     return null;
   };
 
-  const handlePress = (index: number): void => {
-    if (gameMatriz[index] || checkWinner(gameMatriz)) return;
+  useEffect(() => {
+    const winner = checkWinner(gameMatriz);
 
-    const newGameMatriz = [...gameMatriz];
-    newGameMatriz[index] = isXNext ? "X" : "O";
-
-    setGameMatriz(newGameMatriz);
-    setIsXNext(!isXNext);
-
-    const winner = checkWinner(newGameMatriz);
     if (winner) {
       Alert.alert(`${winner} ganhou!`);
-    } else if (!newGameMatriz.includes(null)) {
+      winner === "X"
+        ? setPlayerVictory((prev) => prev + 1)
+        : setCpuVictory((prev) => prev + 1);
+    } else if (!gameMatriz.includes(null)) {
       Alert.alert("Empate!");
+    } else if (!isXNext) {
+      setTimeout(randomCpuMove, 1000);
     }
+  }, [gameMatriz]);
+
+  const handlePress = (index: number): void => {
+    if (gameMatriz[index] || checkWinner(gameMatriz) || !isXNext) return;
+
+    const newGameMatriz = [...gameMatriz];
+    newGameMatriz[index] = "X";
+    setGameMatriz(newGameMatriz);
+    setIsXNext(false);
+  };
+
+  const randomCpuMove = (): void => {
+    const availableMoves = gameMatriz
+      .map((value, index) => (value === null ? index : null))
+      .filter((value): value is number => value !== null);
+
+    if (availableMoves.length === 0) return;
+
+    const randomIndex =
+      availableMoves[Math.floor(Math.random() * availableMoves.length)];
+
+    const newGameMatriz = [...gameMatriz];
+    newGameMatriz[randomIndex] = "O";
+    setGameMatriz(newGameMatriz);
+    setIsXNext(true);
   };
 
   const restartGame = (): void => {
@@ -53,6 +78,9 @@ export default function Index() {
 
   return (
     <View style={styles.main}>
+      <View style={styles.containerScoreBoard}>
+        <Text style={styles.scoreBoardText}>👤 VC: {playerVictory}   |   🤖 CPU: {cpuVictory}</Text>
+      </View>
       <View style={styles.containerBoard}>
         {[0, 3, 6].map((startIndex) => (
           <View key={startIndex} style={styles.row}>
@@ -62,6 +90,7 @@ export default function Index() {
           </View>
         ))}
       </View>
+        <Text style={styles.TurnText}>Vez do <Text style={{color: "#ffcc54"}}>{isXNext ? "Jogador" : "Computador"}</Text></Text>
       <View style={styles.containerResetButton}>
         <TouchableOpacity style={styles.resetButton} onPress={restartGame} activeOpacity={0.8}>
           <Text style={styles.resetButtonText}>Reiniciar Jogo</Text>
@@ -78,17 +107,45 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+
+  containerScoreBoard: {
+    backgroundColor: "#2c2c2c",
+    borderRadius: 12,
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    marginBottom: 30,
+    borderWidth: 2,
+    borderColor: "#444",
+    alignItems: "center",
+  },
+  
+  scoreBoardText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+    textAlign: "center",
+  },  
+
   containerBoard: {
     backgroundColor: "#333",
     borderRadius: 16,
     padding: 20,
     marginBottom: 30,
   },
+
   row: {
     flexDirection: "row",
     justifyContent: "space-between",
     marginBottom: 5,
   },
+
+  TurnText: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "#fff",
+    textAlign: "center",
+  },
+
   containerResetButton: {
     marginTop: 20,
     alignItems: "center",
